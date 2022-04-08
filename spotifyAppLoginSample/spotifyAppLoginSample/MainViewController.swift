f75691ad9e4e998cd448b1d9ca34da55c4bae3f2
김태영 <99245719+Taeshiki@users.noreply.github.com>
2022-04-08 18:08:11 +0900

    기본 UI구성

---
+++ spotifyAppLoginSample/spotifyAppLoginSample/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  spotifyAppLoginSample
+//
+//  Created by 김태영 on 2022/04/08.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+}
